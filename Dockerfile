5e6751ebec4ad5f56e17412ed129eaa5502606b7
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2021-02-18 18:10:45 +0530

    init 1

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8-alpine
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV DEBUG 0
+
+RUN apk update \
+    && apk add virtual build-deps gcc python3-dev musl-dev  mariadb-client
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD python manage.py runserver 0.0.0.0:$PORT
--- erp_project/settings.py
+++ erp_project/settings.py
@@ -80,12 +80,23 @@ WSGI_APPLICATION = 'erp_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_HOST"),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
